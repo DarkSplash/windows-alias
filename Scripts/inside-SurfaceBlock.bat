39460435055f4fc88990bf9a578f37024a85e870
Alexander Swinford <amswinford@gmail.com>
2022-11-03 12:48:26 -0500

    added network adapter scripts
    used during ASA 2022 replacement

---
+++ Scripts/inside-SurfaceBlock.bat
@@ -0,0 +1,6 @@
+@echo off
+set adapterName=Ethernet 3
+
+netsh interface ip set address "%adapterName%" static 10.0.14.15 255.255.255.0 10.0.14.1
+netsh interface ip add dns "%adapterName%" 1.1.1.1
+netsh interface ip add dns "%adapterName%" 1.0.0.1 index=2
